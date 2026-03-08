70fe202bd2bf7a93077b809191c725704804364b
praneshnikhar <praneshnikhar@gmail.com>
2026-03-08 23:08:33 +0530

    -

--- src/components/RegistryView.tsx
+++ src/components/RegistryView.tsx
@@ -290,6 +290,17 @@ import DocumentUpload from "./DocumentUpload";
 import MapDraw from "@/components/MapDraw";
 import * as turf from "@turf/turf";
 
+// interface LandParcel {
+//   id: string;
+//   landId: string;
+//   owner: string;
+//   location: string;
+//   coordinates: { lat: number; lng: number };
+//   polygonCoords?: [number, number][];
+//   documentUrl?: string;
+//   timestamp: string;
+// }
+
 interface LandParcel {
   id: string;
   landId: string;
